88e9d8dfe0432ed6b7265f5e57a26284bad2223c
Jan Krause <jk.net@protonmail.ch>
2022-02-16 08:54:55 +0100

    updated cli tool

--- cli.py
+++ cli.py
@@ -11,21 +11,32 @@
 # First release: 2022-02-02
 
 import os
-import sys
 import pprint
 import configparser
+import argparse
 
 from archive2dna import package
 
-usage = '''cli.py ACTION FILE_IN FILE_OUT [PACKAGE_ID]
-           action : encode | decode\n'''
-
 pp = pprint.PrettyPrinter(depth=6)
+
+# Parse arguments
+usage = '''cli.py ACTION FILE_IN FILE_OUT [--id package_id] [--config config_section]
+           action : encode | decode\n'''
+parser = argparse.ArgumentParser(description='Encode/decode information package to DNA.')
+parser.add_argument('action', help='encode | decode .')
+parser.add_argument('input_file', help='Input file.')
+parser.add_argument('output_file', help='Output file.')
+parser.add_argument('--id', dest='package_id', help='Information package ID, used to generate the primer')
+parser.add_argument('--config', help='Config set to be used, e.g. DEFAULT or BIG (see config.ini)')
+args = parser.parse_args()
 
 # read config
 cfg = configparser.ConfigParser()
 cfg.read('config.ini')
-cfg_set = 'DEFAULT'
+if args.config == None:
+    cfg_set = 'DEFAULT'
+else:
+    cfg_set = args.config
 primer_length = int(cfg[cfg_set]['primer_length'])
 mi = int(cfg[cfg_set]['mi'])
 mo = int(cfg[cfg_set]['mo'])
@@ -34,25 +45,20 @@ index_positions = int(cfg[cfg_set]['index_positions'])
 N = int(cfg[cfg_set]['N'])
 K = int(cfg[cfg_set]['K'])
 target_redundancy = float(cfg[cfg_set]['target_redundancy'])
-if cfg[cfg_set]['target_redundancy'] == 'False':
+if cfg[cfg_set]['auto_zip'] == 'False':
     auto_zip = False
 else:
     auto_zip = True 
 
-if len(sys.argv)==5:
-    package_id = sys.argv[4]
-else:
-    package_id = None
+
+if args.package_id == None:
     primer_length = 0
     
-if sys.argv[1] in ['-h', '--help']:
-   print(usage)
-    
-elif sys.argv[1]=='encode':
-    binary = sys.argv[2]
-    dna = sys.argv[3]
+if args.action=='encode':
+    binary = args.input_file
+    dna = args.output_file
     binary_data = open(binary, 'rb').read()
-    c = package.Container( package_id = package_id, 
+    c = package.Container( package_id = args.package_id, 
                            primer_length = primer_length,
                            mi = mi,
                            mo = mo,
@@ -70,12 +76,19 @@ elif sys.argv[1]=='encode':
     open(dna, 'w').write( text )
     pp.pprint(c.compute_stats())
     
-elif sys.argv[1]=='decode':
-    binary = sys.argv[3]
-    dna = sys.argv[2]
-    c = package.Container( package_id=package_id, 
-                           primer_length=primer_length,
-                           mo = mo )
+elif args.action=='decode':
+    binary = args.output_file
+    dna = args.input_file
+    c = package.Container( package_id = args.package_id, 
+                           primer_length = primer_length,
+                           mi = mi,
+                           mo = mo,
+                           index_length = index_length,
+                           index_positions = index_positions,
+                           N = N,
+                           K = K,
+                           target_redundancy = target_redundancy,
+                           auto_zip = auto_zip)
     text = open(dna, 'r').read()
     c.load_dna(text)
     c.check_and_correct_logical_redundancy()
